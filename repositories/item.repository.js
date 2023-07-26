f15c7d275947a2087d69198c6ff2cfff9c015b1c
wonbin choi <fnffnqls@naver.com>
2023-07-26 16:55:37 +0900

    fix: 상품 관리 api 레파지토리 수정

--- repositories/item.repository.js
+++ repositories/item.repository.js
@@ -2,7 +2,16 @@ const { Item } = require("../models");
 
 class ItemsRepository {
   postItem = async (name, price, type) => {
-    await Item.create({ name, price, type });
+    const item = await Item.findOne({ where: { name } });
+    console.log(item.amount);
+    if (!item) {
+      await Item.create({ name, price, type });
+      item.amount += 1;
+      await item.save();
+    } else {
+      item.amount += 1;
+      await item.save();
+    }
   };
 }
 

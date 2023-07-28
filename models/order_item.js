d4580491b6dcb800f827364a89cee496fccb8308
wonbin choi <fnffnqls@naver.com>
2023-07-28 11:27:25 +0900

    style: 변수 케이스 변경 itemId -> item_id

--- models/order_item.js
+++ models/order_item.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
         primaryKey: true,
         type: DataTypes.INTEGER,
       },
-      itemId: {
+      item_id: {
         type: DataTypes.BIGINT,
       },
       amount: {
